bde8c120635ba7c7cbb86152c04cbd37174c31af
Dhanesh-Arun <dhaneshshaesh12410@gmail.com>
2024-08-07 18:27:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import './tailwind.css';
+import Form from './Components/Form';
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Form></Form>
+      </div>
+  );
+}
+
+export default App;
